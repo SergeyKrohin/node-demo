71fe8782cd143c060d23e798fa48d4c5f8997df4
sergeykrohin <sergeykrohin86@gmail.com>
2020-05-31 11:39:23 +0200

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+function onRequest(req, res){
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, (error, data) => {
+        if(error) {
+            res.writeHead(404);
+            res.write('File not found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+}
+
+http.createServer(onRequest).listen(8000);
+
+
